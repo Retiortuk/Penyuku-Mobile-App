bc0db251ef907c91b658d333569f5ffb988c9b7b
RetiortUK <aauliagazzam@gmail.com>
2025-10-27 20:50:48 +0700

    before Merge

---
+++ lib/splash_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () => _navigateToLogin());
+  }
+
+  _navigateToLogin() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const Placeholder()),
+    );
+  }
+
+  Widget _buildCircle({
+    required double size,
+    required Color color,
+    double? top,
+    double? left,
+    double? right,
+    double? bottom,
+  }) {
+    return Positioned(
+      top: top,     
+      left: left,   
+      right: right, 
+      bottom: bottom,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/background.png', fit: BoxFit.cover),
+
+          _buildCircle(
+            size: 225,
+            color: Colors.white.withOpacity(0.75),
+            top: -120, 
+            right: -80, 
+          ),
+
+          _buildCircle(
+            size: 80,
+            color: Colors.white.withOpacity(0.75),
+            top: 525, 
+            left: 120, 
+          ),
+
+          _buildCircle(
+            size: 225,
+            color: Colors.white.withOpacity(0.75),
+            bottom: -100,
+            left: -80, 
+          ),
+
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/logo-splashscreen.png',
+                  width: 200,
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
